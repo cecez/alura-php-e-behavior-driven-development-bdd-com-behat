87bcaf6ac69cfc326374905555ddee3b62bf39c2
Cezar Rosa <cecez86@gmail.com>
2020-12-17 10:13:33 -0300

    * instalação e configuração do Behat no repositório * esqueleto para testes com Behat * início dos testes para formação de cursos

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,61 @@
+<?php
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @When eu tentar criar uma formação com a descrição :arg1
+     */
+    public function euTentarCriarUmaFormacaoComADescricao($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then eu vou ver a seguinte mensagem e erro :arg1
+     */
+    public function euVouVerASeguinteMensagemEErro($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given que estou conectado ao banco de dados
+     */
+    public function queEstouConectadoAoBancoDeDados()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When tento criar uma nova formação com a descrição :arg1
+     */
+    public function tentoCriarUmaNovaFormacaoComADescricao($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then se eu buscar no banco, devo encontrar essa formação
+     */
+    public function seEuBuscarNoBancoDevoEncontrarEssaFormacao()
+    {
+        throw new PendingException();
+    }
+}
